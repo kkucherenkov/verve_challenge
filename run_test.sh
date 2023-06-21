3ada0ffe85cc076e187da54efdc3197bffa7bc83
Kirill Kucherenkov <kirill.kucherenkov@gmail.com>
2023-06-21 15:44:59 +0400

    feat(load balancer): add simple load balancer via nginx

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 docker run --network=backend \
-        --rm skandyla/wrk -t4 -c4 -d60s http://app:8080/promotions/33ae574d-60d3-4d65-9deb-f25fd04a229a
+        --rm skandyla/wrk -t4 -c4 -d15s http://ngnix-server:1321/promotions/33ae574d-60d3-4d65-9deb-f25fd04a229a
+
+docker run --network=backend \
+        --rm skandyla/wrk -t4 -c4 -d15s http://app1:8080/promotions/33ae574d-60d3-4d65-9deb-f25fd04a229a
